8479140b6f3cfb4401feef95587c0d51149ed29b
unknown <farhanalktr91@gmail.com>
2025-06-23 11:32:25 +0700

    feat: add api getTaskUser

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -3,6 +3,7 @@ import {
   getTasks,
   createTask,
   updateTaskStatus,
+  getUserTasks,
 } from "../controllers/taskController";
 
 const router = Router();
@@ -10,5 +11,5 @@ const router = Router();
 router.get("/", getTasks);
 router.post("/", createTask);
 router.patch("/:taskId/status", updateTaskStatus);
-
+router.get("/user/:userId", getUserTasks);
 export default router;
